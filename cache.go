39ed41ee9ece9abe1956e0d52777305f7c440641
HasmikMargaryan <hasmik_margaryan@epam.com>
2022-06-15 11:45:48 -0700

    Initial solution

--- cache.go
+++ cache.go
@@ -2,22 +2,45 @@ package cache
 
 import "time"
 
+type Data struct {
+	key      string
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	CacheData []Data
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	for _, data := range c.CacheData {
+		if data.key == key && data.deadline.Before(time.Now()) {
+			return data.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	newData := Data{key, value, time.Now()}
+	c.CacheData = append(c.CacheData, newData)
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var notExpiredKeys []string
+	for _, data := range c.CacheData {
+		if data.deadline.Before(time.Now()) {
+			notExpiredKeys = append(notExpiredKeys, data.key)
+		}
+	}
+	return notExpiredKeys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	newData := Data{key, value, deadline}
+	c.CacheData = append(c.CacheData, newData)
 }
